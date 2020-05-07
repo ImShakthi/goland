7253c8b97e77278178f41f48d8a959ae0dfb8253
Sakthivel Balasubramaniam <sakthivel.b@thoughtworks.com>
2020-05-08 00:18:48 +0530

    Upadte select query

--- repositories/upvotes.go
+++ repositories/upvotes.go
@@ -19,21 +19,12 @@ func NewUpVotesRepo(db *gorm.DB) UpVotesRepo {
 }
 
 const (
-	GetUpVoteCountQuery = "SELECT COUNT(*) FROM votes v, pages p, sites s " +
-		" WHERE v.page_id = p.page_id " +
-		" AND p.site_id = s.site_id " +
-		" AND p.site_id = ? " +
-		" AND p.page_id = ? "
+	GetUpVoteCountQuery = `SELECT COUNT(v.vote_id) FROM votes v, pages p, sites s 
+				WHERE v.page_id = p.page_id AND p.site_id = s.site_id AND p.site_id = ? AND p.page_id = ? `
 )
 
 func (r *upVotesRepo) GetUpVoteCounts(siteId string, pageId string) (count int, err error) {
-	type result struct {
-		Count int
-	}
-
-	var res result
-	r.db.Raw(GetUpVoteCountQuery, siteId, pageId).Scan(&res)
-	count = res.Count
+	r.db.Raw(GetUpVoteCountQuery, siteId, pageId).Scan(&count)
 
 	//r.db.Table("votes").Where("page_id = ?", pageId).Count(&count)
 
